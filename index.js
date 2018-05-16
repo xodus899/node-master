ae21c11e89a2c5cb923c9e33f90f3cdafbecd9ed
Christopher Justice <justcd@mail.broward.edu>
2018-05-16 13:22:29 -0400

    Add route handler

--- index.js
+++ index.js
@@ -39,6 +39,32 @@ const server = http.createServer(function(req,res) {
   });
   req.on('end',function() {
     buffer += decoder.end();
+
+    // chose request for hander, if not found use not found handler
+
+    const chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : rotuer[notFound];
+
+    //construct data object to send to the handler
+
+    const data = {
+      'trimmedPath': trimmedPath,
+      'queryStringObject': queryStringObject,
+      'method': method,
+      'headers': headers,
+      'payload': buffer
+    };
+
+    //route to chosen handler
+
+    chosenHandler(data,function(statusCode,payload) {
+      // use status code callbacked by handler or default 200
+      statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+      // use payload called back by payload or default to empty object
+      payload = typeof(payload) == 'object' ? payload : {};
+    })
+
+
+
     // send response
     res.end("hello world\n");
 
@@ -53,3 +79,76 @@ const server = http.createServer(function(req,res) {
 server.listen(3000,function() {
   console.log("Server is listening on port 3000");
 });
+
+  // define handlers
+
+  var handlers = {};
+
+  //sample handler
+  handlers.sample = function(data,callback) {
+    // callback http status code and payload object
+    callback(406,{'name: sample handle'});
+  };
+
+  //not found handlers
+  handlers.notFound = function(data,callback) {
+    callback(404);
+  };
+
+// define a request router
+
+var router = {
+  'sample': handlers.sample
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
