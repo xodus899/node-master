746ab948213f498045e5390d2a9ce15b184447d4
Christopher Justice <justcd@mail.broward.edu>
2018-05-16 11:13:03 -0400

    Create inital server

---
+++ index.js
@@ -0,0 +1,20 @@
+//primary file for api
+
+
+
+
+
+//dependencies
+const http = require("http");
+
+
+// server should respond to all requests in string format
+const server = http.createServer(function(req,res) {
+  res.end("hello world\n");
+})
+
+
+// start server listen on port 3000
+server.listen(3000,function() {
+  console.log("Server is listening on port 3000");
+})
