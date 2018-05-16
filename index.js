2a107aebaf30f336988abf2cabcc8144b574c1a8
Christopher Justice <justcd@mail.broward.edu>
2018-05-16 11:30:22 -0400

    Add url parse/trimmed

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
 
 
 
-
-
 //dependencies
 const http = require("http");
 const url = require("url");
@@ -11,10 +9,25 @@ const url = require("url");
 // server should respond to all requests in string format
 const server = http.createServer(function(req,res) {
   res.end("hello world\n");
+
+  // get url and parse
+
+  const parsedUrl = url.parse(req.url,true);
+
+  //get url path
+
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g,'')
+  // send response
+
+
+  // log path user was asking
+
+
+  // start server listen on port 3000
 });
 
 
-// start server listen on port 3000
 server.listen(3000,function() {
   console.log("Server is listening on port 3000");
 });
