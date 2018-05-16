f6841a16d243a3b2fb7c3a02f75e9cb917cb0735
Christopher Justice <justcd@mail.broward.edu>
2018-05-16 12:43:07 -0400

    Add payload

--- index.js
+++ index.js
@@ -5,10 +5,10 @@
 //dependencies
 const http = require("http");
 const url = require("url");
+const stringDecoder = require("string_decoder").StringDecoder;
 
 // server should respond to all requests in string format
 const server = http.createServer(function(req,res) {
-  res.end("hello world\n");
 
   // get url and parse
 
@@ -19,23 +19,36 @@ const server = http.createServer(function(req,res) {
   const path = parsedUrl.pathname;
   const trimmedPath = path.replace(/^\/+|\/+$/g,'');
 
+  // get query string
+  const queryStringObject = parsedUrl.query;
+
   // get http method
 
   const method = req.method.toUpperCase();
 
-  // send response
+  // get headers as object
 
+  const headers = req.headers;
 
-  // log path user was asking
-  if (trimmedPath === "") {
-    console.log ("User requested homepage", trimmedPath+ "with method: " + method);
-  } else {
-    console.log("User path, " + trimmedPath);
-  }
+  // get the payload if any
 
-  // start server listen on port 3000
+  const decoder = new stringDecoder('utf-8');
+  var buffer = '';
+  req.on('data',function(data) {
+    buffer += decoder.write(data);
+  });
+  req.on('end',function() {
+    buffer += decoder.end();
+    // send response
+    res.end("hello world\n");
+
+    // log path user was asking
+    console.log("Request received with payload:", buffer);
+  });
+
 });
 
+// start server listen on port 3000
 
 server.listen(3000,function() {
   console.log("Server is listening on port 3000");
