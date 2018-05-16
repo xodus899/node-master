c1bb759488249e09e57cb3f50153bcaf171a1977
Christopher Justice <justcd@mail.broward.edu>
2018-05-16 11:21:18 -0400

    Add url parse for api

--- index.js
+++ index.js
@@ -6,15 +6,15 @@
 
 //dependencies
 const http = require("http");
-
+const url = require("url");
 
 // server should respond to all requests in string format
 const server = http.createServer(function(req,res) {
   res.end("hello world\n");
-})
+});
 
 
 // start server listen on port 3000
 server.listen(3000,function() {
   console.log("Server is listening on port 3000");
-})
+});
